b8a2cf01d1c4ae32d100a9186609d0a8009944db
Eduard Maghakyan <edi.maghakyan@gmail.com>
2021-08-23 14:40:13 +0200

    Extend cli tests

--- src/tests/interface/cli/test_cli.py
+++ src/tests/interface/cli/test_cli.py
@@ -1,7 +1,7 @@
 from click.testing import CliRunner
 
-from algo_api.algorithms import fib_iter
-from algo_api.interface.cli.algorithms import invoke_fibonacci
+from algo_api.algorithms import ackermann, factorial, fib_iter
+from algo_api.interface.cli.algorithms import invoke_ackermann, invoke_factorial, invoke_fibonacci
 
 
 def test_fibonacci_cli():
@@ -10,5 +10,26 @@ def test_fibonacci_cli():
     result = runner.invoke(invoke_fibonacci, [str(input)])
     assert result.exit_code == 0
     assert fib_iter(input) == int(
+        result.output
+    ), f"Got wrong result from cli for {input} got {result.output}"
+
+
+def test_ackermann_cli():
+    n = 3
+    m = 1
+    runner = CliRunner()
+    result = runner.invoke(invoke_ackermann, [str(m), str(n)])
+    assert result.exit_code == 0
+    assert ackermann(m, n) == int(
+        result.output
+    ), f"Got wrong result from cli for {m, n} got {result.output}"
+
+
+def test_factorial_cli():
+    input = 12
+    runner = CliRunner()
+    result = runner.invoke(invoke_factorial, [str(input)])
+    assert result.exit_code == 0
+    assert factorial(input) == int(
         result.output
     ), f"Got wrong result from cli for {input} got {result.output}"
